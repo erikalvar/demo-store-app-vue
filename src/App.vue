7914ef870fc8a68a051140c1f8f19ac270406245
erikalvar <erikalvarbass@gmail.com>
2020-12-10 16:50:31 -0500

    adds bootstrap navbar and hero

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,80 @@
 <template>
   <div id="app">
     <!-- header -->
-    <header class=header>
-      <div class="nav">
+    <header class="header">
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <router-link class="navbar-brand" to="/">THE BASS SHOP</router-link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/new-arrivals"
+                  >NEW ARRIVALS</router-link
+                >
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/uprights"
+                  >UPRIGHTS</router-link
+                >
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/electrics"
+                  >ELECTRICS</router-link
+                >
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/bows">BOWS</router-link>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  ACCESSORIES
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Strings</a></li>
+                  <li><a class="dropdown-item" href="#">Cases</a></li>
+                  <li><a class="dropdown-item" href="#">Tuners and Metronomes</a></li>
+                  <li><a class="dropdown-item" href="#">Pickups</a></li>
+                  <li><a class="dropdown-item" href="#">Amps</a></li>
+                  <li><a class="dropdown-item" href="#">Parts</a></li>
+                  <li><a class="dropdown-item" href="#">Books/DVDs/Audio</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <!-- <div class="nav">
         <div class="nav-logo">
           <router-link to="/">THE BASS SHOP</router-link>
         </div>
@@ -11,14 +83,14 @@
         <router-link to="/electrics">ELECTRICS</router-link> |
         <router-link to="/bows">BOWS</router-link> |
         <router-link to="/accessories">ACCESSORIES</router-link> 
-      </div>
+      </div> -->
     </header>
     <!-- end header -->
 
-    <router-view/>
+    <router-view />
 
     <!-- footer -->
-    <footer class="footer">
+    <footer class="footer fixed-bottom">
       <p>Test Footer</p>
     </footer>
     <!-- end footer -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,68 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div
+      id="carouselExampleIndicators"
+      class="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <ol class="carousel-indicators">
+        <li
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          class="active"
+        ></li>
+        <li
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+        ></li>
+        <li
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+        ></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="../../public/images/jbasswide.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src="../../public/images/jbasswide.jpg" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src="../../public/images/jbasswide.jpg" class="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleIndicators"
+        role="button"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </a>
+    </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
 export default {
-  data: function() {
+  data: function () {
     return {
-      message: "Welcome to the Bass Shop!"
+      message: "Welcome to the Bass Shop!",
     };
   },
-  created: function() {},
-  methods: {}
+  created: function () {},
+  methods: {},
 };
 </script>
