7ef8a5b2426c20d5c7449cab718bb3c7ae32d65a
erikalvar <erikalvarbass@gmail.com>
2020-12-15 16:59:59 -0500

    adds container and column to products show

--- src/views/ProductsShow.vue
+++ src/views/ProductsShow.vue
@@ -4,7 +4,7 @@
 
     <!-- hero -->
     <div class="bg-light p-5 rounded-lg m-3">
-      <h1 class="display-4">{{ message}}</h1>
+      <h1 class="display-4">{{ message }}</h1>
       <p class="lead">
         This is a simple hero unit, a simple jumbotron-style component for
         calling extra attention to featured content or information.
@@ -18,6 +18,24 @@
     </div>
     <!-- end hero -->
 
+    <!-- product container -->
+    <div class="container">
+      <div class="row align-items-start">
+        <!-- left column -->
+        <div class="col">
+          <img src="../../public/images/kay_bass_viol_1954_01.jpg" class="card-img-top" alt="..." />
+        </div>
+        <!-- right column -->
+        <div class="col">
+          <h1>{{ product.year }} {{ product.name }} {{ product.model }}</h1>
+          <p>{{ product.description}}</p>
+          <p>{{ product.specs}}</p>
+          <p>${{ product.price}}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+    <!-- end product container -->
   </div>
 </template>
 
--- src/views/UprightsIndex.vue
+++ src/views/UprightsIndex.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="uprights-index">
-    <h1>{{ products.length}}</h1>
+    <h1>{{ products.length }}</h1>
 
     <!-- hero -->
     <div class="bg-light p-5 rounded-lg m-3">
-      <h1 class="display-4">{{ message}}</h1>
+      <h1 class="display-4">{{ message }}</h1>
       <p class="lead">
         This is a simple hero unit, a simple jumbotron-style component for
         calling extra attention to featured content or information.
@@ -20,21 +20,25 @@
 
     <!-- cards -->
     <div class="container-fluid">
-      <div class="row row-cols-2 row-cols-md-4 g-4" >
-      <div class="col" v-for="product in products">
-        <router-link v-bind:to="`/products/${product.id}`">
-          <div class="card h-100">
-          <img src="../../public/images/kay_bass_viol_1954_01.jpg" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">{{ product.year  }}  {{ product.name }} {{ product.model }}</h5>
-            <p class="card-text">
-              {{ product.price}}
-            </p>
-          </div>
-          </div>
-        </router-link>
+      <div class="row row-cols-2 row-cols-md-4 g-4">
+        <div class="col" v-for="product in products">
+          <router-link v-bind:to="`/products/${product.id}`">
+            <div class="card h-100">
+              <img
+                src="../../public/images/kay_bass_viol_1954_01.jpg"
+                class="card-img-top"
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  {{ product.year }} {{ product.name }} {{ product.model }}
+                </h5>
+                <p class="card-text">${{ product.price }}</p>
+              </div>
+            </div>
+          </router-link>
+        </div>
       </div>
-    </div>
     </div>
     <!-- end cards -->
   </div>
